87f69b4bce32cf50b86cae2d10991e31fc49a0e1
spooky-noodle <christopherman2cool@gmail.com>
2020-12-17 12:25:08 -0700

    implemented dirty loop

--- python/Runeterra Art Grabber/lor_art_grabber.py
+++ python/Runeterra Art Grabber/lor_art_grabber.py
@@ -2,6 +2,30 @@ import os
 import requests as rq
 import concurrent.futures
 from PIL import Image
+import time
+
+"""
+Example URL = https://cdn-lor.mobalytics.gg/production/images/set2/en_us/img/card/game/02BW022T2-full.webp
+
+02 = {self.card_set:02}
+"BW" = region
+022 = {number:03}
+T2 = "T" + suffix_num
+
+Pseudocode:
+
+For each region in region_list
+    number starts at 1
+        card_code = card_set + region + number
+        check if card exists at url
+            if card does exist, append it to card list, suffix = 1
+            if card doesn't exist and number > 5, go to next region
+            card_code = card_set + region + number + "T" + suffix
+            check if card exists with suffix
+                if card w/ suffix does exist, append to card list, go to next suffix
+                if card w/ suffix doesn't exist, go to next number
+"""
+
 
 region_list = ['BW', 'DE', 'FR', 'IO', 'MT', 'NX', 'PZ', 'SI']
 
@@ -10,15 +34,13 @@ class ImageSnatcher:
     A class using requests to download images from a website.
     
     Attributes:
-        url_base (str): the base string of the URL where all of the flags are located (def = 'https://cdn-lor.mobalytics.gg/production/images/set2/en_us/img/card/game/02')
-        flag_file (str): the name of the file where the flag codes are stored (def = 'flags.txt')
-        loaded_bytes (int): the number of bytes that have been downloaded by the snatcher
-    """
+        url_base (str): the base string of the URL where all of the images are located (def = 'https://cdn-lor.mobalytics.gg/production/images/set2/en_us/img/card/game/02')
+     """
 
     def __init__(self, img_front  = 'https://cdn-lor.mobalytics.gg/production/images/set', card_set = 2):
         """Reads in the file of flag codes, and chooses 10 random codes for download purposes."""
         self.card_set = card_set
-        self.url_base = (f"{img_front}{card_set}/en_us/img/card/game/0{card_set}")
+        self.url_base = (f"{img_front}{card_set}/en_us/img/card/game/{self.card_set:02}")
         print(f"Created image snatcher!")
         self.all_codes = []
         if os.path.isdir(f'set_{card_set}') == False:
@@ -40,15 +62,30 @@ class ImageSnatcher:
             file.write(flag.content)
         print(f"Successfully saved {code.upper()} flag ({flag_bytes} bytes) to {code}.gif")
     
-    def find_cards(self):
+    def download_cards(self):
         """
         Finds all valid card combinations for this set
 
         Returns valid_cards list
         """
-        self.valid_cards = []
-        for reg in region_list:
-            pass
+        test_list = [True, False, True, True, True, False, False, True, True, True, False]
+        sub_list = 
+        index = -1
+        for region in region_list:
+            number = 1
+            while index < (len(test_list) - 1):
+                card_code = (f"{region}{number:03}")
+                index += 1
+                if test_list[index] == True:
+                    print(f"{card_code}: True")
+                    number += 1
+                else:
+                    print(f"{card_code}: False")
+                    break
+                        
+
+
+            
 
     
     def download_all_flags(self):
@@ -59,12 +96,8 @@ class ImageSnatcher:
         print("Successfully downloaded all images!")
 
 def main():
-    # snatcher = ImageSnatcher()
-    # snatcher.download_all_flags()
-    unit = rq.get('https://cdn-lor.mobalytics.gg/production/images/set1/en_us/img/card/game/01IO009-full.webp')
-    unit2 = rq.get('https://cdn-lor.mobalytics.gg/production/images/set1/en_us/img/card/game/01IO009T1-full.webp')
-    spell = rq.get('https://cdn-lor.mobalytics.gg/production/images/set1/en_us/img/card/game/01IO009T3-full.webp')
-    print(f"Zed Bytes: {len(unit.content)}, Shadow Bytes: {len(unit2.content)}, Spell Bytes: {len(spell.content)}")
+    thingie = ImageSnatcher()
+    thingie.download_cards()
 
 if __name__ == "__main__":
     main()
