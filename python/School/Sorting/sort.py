e941a2607c3754bf8fb82faa2305b6dbf9699613
spooky-noodle <christopherman2cool@gmail.com>
2021-02-15 13:26:39 -0700

    established skeleton functions

---
+++ python/School/Sorting/sort.py
@@ -0,0 +1,20 @@
+def selection_sort():
+    pass
+
+def insertion_sort():
+    pass
+
+def mergesort():
+    pass
+
+def quicksort():
+    pass
+
+def is_sorted():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
