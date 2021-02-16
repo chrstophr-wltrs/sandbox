c6a084ef2bac8e56de54bec047f75ec2aa9089cd
spooky-noodle <christopherman2cool@gmail.com>
2021-02-15 17:35:22 -0700

    Implemented selection_sort function

--- python/School/Sorting/sort.py
+++ python/School/Sorting/sort.py
@@ -1,13 +1,35 @@
 from random import seed, sample
 from time import perf_counter
 from typing import Callable
-from statistics import median
 
 seed(27538649)
 
+def selection_sort(lyst: list[int], left: int = -1):
+    """
+    Sorts the list using a selection sort
 
-def selection_sort(lyst: list):
-    pass
+    NOTE: MODIFIES THE BASE LIST
+
+    Parameters:
+        lyst (list): a list of integers
+        left (int): the leftmost index of the boundary of the portion of the list that's being sorted (default = -1)
+    """
+    while True: 
+        # We've sorted the entire list, time to break the loop
+        if left == len(lyst) - 1:
+            break
+        
+        # Increment to the next left boundary
+        left += 1
+        
+        # Search for the index of the minimum value
+        mindex = lyst.index(min(lyst[left:]))
+
+        # Swap the minimum value to the end of the sorted boundary
+        lyst[left], lyst[mindex] = lyst[mindex], lyst[left]
+    
+    return(lyst)
+
 
 def insertion_sort(lyst: list):
     pass
@@ -67,14 +89,14 @@ def quicksort(lyst: list[int], left: int = 0, right: int = None):
 def is_sorted(lyst: list):
     """returns True if the list is sorted, returns False if list is not sorted."""
     for i in range(1, len(lyst)):
-        if lyst(i - 1) > lyst(i):
+        if lyst[i - 1] > lyst[i]:
             return False
     return True
 
 def main():
     """Main function, does the tests, prints out how well each sort does."""
     
-    def time_test(lyst: tuple, sort_func: Callable):
+    def time_test(test_tup: tuple, sort_func: Callable):
         """
         Helper function for main, tests the sort speed of a function
         
@@ -82,7 +104,7 @@ def main():
             lyst (tuple): a large list of elements, unsorted
             sort_func (function): the function that is being tested
         """
-        test_list = list(lyst)
+        test_list = list(test_tup)
         print(f"Beginning test for {sort_func.__name__} function...")
         start_time = perf_counter()
         sorted_list = sort_func(test_list)
@@ -91,20 +113,22 @@ def main():
             sort_result = "sorted"
         else:
             sort_result = "not sorted"
-        print(f"Time for {sort_func.__name__} to sort: {stop_time - start_time:.7f} seconds ({sort_result}!)")
+        print(f"Time for {sort_func.__name__} to sort: {stop_time - start_time:.4f} seconds ({sort_result}!)")
         return sorted_list
     
     # Generate list of random integers for sort testing
     print("Generating list for sort testing...")
     gen_start = perf_counter()
-    messy_tuple = tuple(sample(range(100000), 50000))
+    messy_tuple = tuple(sample(range(50000), 25000))
     print(f"List generated in {perf_counter() - gen_start:.2f} seconds, n = {len(messy_tuple):,}")
+
+    sort_methods = (quicksort, selection_sort)
     
-    sort_methods = (quicksort)
-    
+    mini_list = [3, 7, -5, 4, 18, -9, 13]
+
     # Test each of the sort algorithms
     for func in sort_methods:
-        time_test(func, messy_tuple)
+        time_test(messy_tuple, func)
 
 if __name__ == "__main__":
     main()
