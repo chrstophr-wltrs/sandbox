fe0f6b723356207d8d966dc0918e2675940f87df
spooky-noodle <christopherman2cool@gmail.com>
2021-02-26 12:55:48 -0700

    implemented Course skeleton

---
+++ python/School/Linked List/course.py
@@ -0,0 +1,28 @@
+class Course:
+
+    def __init__(self):
+        self.number = None
+        self.name = None
+        self.credit_hr = None
+        self.grade = None
+        pass
+
+    def number(self):
+        """retrieve Course Number as an integer"""
+        pass
+
+    def name(self):
+        """retrieve Course Name as a string"""
+        pass
+
+    def credit_hr(self):
+        """retrieve Credits as a floating-point number"""
+        pass
+
+    def grade(self):
+        """retrieve Grade as a numeric value in range 4.0 – 0.0"""
+        pass
+
+    def __str__(self):
+        """returns a string representing a single Course"""
+        pass
