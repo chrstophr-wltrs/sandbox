cb41d7bcb48d21c7311ac2b6b6a38038328e632f
spooky-noodle <christopherman2cool@gmail.com>
2021-02-26 13:10:48 -0700

    implemented methods and class docstring

--- python/School/Linked List/course.py
+++ python/School/Linked List/course.py
@@ -1,28 +1,39 @@
 class Course:
+    """
+    Nodes of a linked list
 
-    def __init__(self):
-        self.number = None
-        self.name = None
-        self.credit_hr = None
-        self.grade = None
-        pass
+    Represents a computer science course at Some Univeristy
+
+    Attributes:
+        number(int): the course number, ex: 2300
+        name(str): the course name, ex: "Web Programming"
+        credit_hr(float): the credit hours the course contributes to a degree or certification
+        grade(float): the grade the student has received in the course
+    """
+
+    def __init__(self, number:int = 0, name:str = "", credit_hr:float = 1, grade:float = 0.0):
+        self.number = number
+        self.name = name
+        self.credit_hr = credit_hr
+        self.grade = grade
+        self.next = None
 
     def number(self):
         """retrieve Course Number as an integer"""
-        pass
+        return self.number
 
     def name(self):
         """retrieve Course Name as a string"""
-        pass
+        return self.name
 
     def credit_hr(self):
         """retrieve Credits as a floating-point number"""
-        pass
+        return self.credit_hr
 
     def grade(self):
         """retrieve Grade as a numeric value in range 4.0 – 0.0"""
-        pass
+        return round(self.grade, 1)
 
     def __str__(self):
         """returns a string representing a single Course"""
-        pass
+        return f"cs{self.number} {self.name} Grade:{self.grade:.1f} Credit Hours: {self.credit_hr:.1f}"
