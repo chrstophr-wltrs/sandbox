e450291ac00c523dde4438a5a2a07e1dfe6df099
spooky-noodle <christopherman2cool@gmail.com>
2021-02-26 12:58:32 -0700

    implemented skeleton

---
+++ python/School/Linked List/courselist.py
@@ -0,0 +1,24 @@
+class Courselist:
+
+    def __init__(self):
+        pass
+
+    def insert(self, Course):
+        """insert the specified Course in Course Number ascending order"""
+        pass
+
+    def remove(self, number): 
+        """remove the first occurrence of the specified Course"""
+        pass
+
+    def remove_all(self, number):
+        """removes ALL occurrences of the specified Course"""
+        pass
+
+    def find(self, number):
+        """find the first occurrance of the specified course in the list or return -1"""
+        pass
+
+    def size(self):
+        """return the number of items in the list"""
+        pass
