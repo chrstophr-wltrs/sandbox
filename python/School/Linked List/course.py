f88f1f3cc01b0b094b00bf52cfa97ad79e2c523b
spooky-noodle <christopherman2cool@gmail.com>
2021-02-26 17:41:56 -0700

    changed attribute names to accomodate tests

--- python/School/Linked List/course.py
+++ python/School/Linked List/course.py
@@ -14,29 +14,29 @@ class Course:
     """
 
     def __init__(self, number:int = 0, name:str = "", credit_hr:float = 0.0, grade:float = 0.0):
-        self.number = int(number)
-        self.name = str(name)
-        self.credit_hr = float(credit_hr)
-        self.grade = float(grade)
+        self.num = int(number)
+        self.title = str(name)
+        self.credits = float(credit_hr)
+        self.score = float(grade)
         self.next = None
         self.prev = None
 
     def number(self):
         """retrieve Course Number as an integer"""
-        return self.number
+        return self.num
 
     def name(self):
         """retrieve Course Name as a string"""
-        return self.name
+        return self.title
 
     def credit_hr(self):
         """retrieve Credits as a floating-point number"""
-        return self.credit_hr
+        return self.credits
 
     def grade(self):
         """retrieve Grade as a numeric value in range 4.0 – 0.0"""
-        return round(self.grade, 1)
+        return round(self.score, 1)
 
     def __str__(self):
         """returns a string representing a single Course"""
-        return f"cs{self.number} {self.name} Grade:{self.grade:.1f} Credit Hours: {self.credit_hr:.1f}"
+        return f"cs{self.number()} {self.name()} Grade:{self.grade()} Credit Hours: {self.credit_hr():.1f}"
