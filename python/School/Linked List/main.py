f53d74490a659ed1e6424f40209ac1b2330a2f10
spooky-noodle <christopherman2cool@gmail.com>
2021-02-26 21:39:05 -0700

    reading in file testing

--- python/School/Linked List/main.py
+++ python/School/Linked List/main.py
@@ -1,16 +1,14 @@
 from course import Course
 from courselist import CourseList
-from pylint import epylint as lint
 
 def main():
-    from pylint import epylint as lint
-    import re
-    
-    (pylint_stdout, pylint_stderr) = lint.py_run('course.py', return_std=True)
-    actual = pylint_stdout.getvalue()
-    x = re.findall('at [0-9].[0-9]+', actual)[0]
-    x = float(x.split()[-1])
-    print(x)
+    my_courses = CourseList()
+    with open("data.txt", "r") as file:
+        for line in file:
+            line_list = line.strip().split(",")
+            new_course = Course(line_list[0], line_list[1], line_list[2], line_list[3])
+            my_courses.insert(new_course)
+    print(my_courses)
 
 if __name__ == "__main__":
     main()
