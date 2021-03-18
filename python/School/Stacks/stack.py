44ec6ecad9ba3656cf1694270d2c1220cfe013bd
spooky-noodle <christopherman2cool@gmail.com>
2021-03-18 17:42:05 -0600

    Implemented Stack skeleton

---
+++ python/School/Stacks/stack.py
@@ -0,0 +1,45 @@
+class Stack:
+    """
+    Stack objects, to hold data
+
+    Methods:
+        push(item): push an item onto the stack. Size increases by 1
+        pop(): remove the top item from the stack and return it. Raise an IndexError if the stack is empty
+        top(): return the item on top of the stack without removing it. Raise an IndexError if the stack is empty
+        size(): return the number of items on the stack
+        clear(): empty the stack
+    """
+
+    def __init__(self):
+        pass
+
+    def push(self, item):
+        """
+        push(item): push an item onto the stack. Size increases by 1
+        
+        Parameters:
+            item(any): item to be pushed onto the top of the stack
+        """
+        pass
+
+    def pop(self):
+        """
+        remove the top item from the stack and return it
+        Raise an IndexError if the stack is empty
+        """
+        pass
+
+    def top(self):
+        """
+        return the item on top of the stack without removing it
+        Raise an IndexError if the stack is empty
+        """
+        pass
+
+    def size(self):
+        """return the number of items on the stack"""
+        pass
+
+    def clear(self):
+        """empty the stack"""
+        pass
