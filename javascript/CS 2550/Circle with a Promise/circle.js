dbf72832689646ddb1b7759b2c97af0aafe1ce43
spooky-noodle <christopherman2cool@gmail.com>
2021-04-02 14:51:55 -0600

    first attempt

--- javascript/CS 2550/Circle with a Promise/circle.js
+++ javascript/CS 2550/Circle with a Promise/circle.js
@@ -24,7 +24,12 @@ window.addEventListener("DOMContentLoaded", function () {
 
 function showCircleClick() {
    // TODO: Add modifications here
-   showCircle(160, 180, 120);
+   let circlePromise = showCircle(160, 180, 120);
+   circlePromise.then((div) => {
+      div.innerHTML = "Ta da!"
+   }).catch((rejectStr) => {
+      window.alert(rejectStr)
+   })
 }
 
 // Do not modify the code below
