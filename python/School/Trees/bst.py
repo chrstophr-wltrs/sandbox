8880af0629dce48a8a6934e945341ad2bed6755f
spooky-noodle <christopherman2cool@gmail.com>
2021-04-03 15:10:43 -0600

    Implement BST skeleton

---
+++ python/School/Trees/bst.py
@@ -0,0 +1,68 @@
+"""
+Project: Binary Search Trees
+Author: Chris Walters
+Course: CS 2550
+Date: 4/3/2021
+
+Description: the bst module, used for testing!
+"""
+
+class BST():
+    """
+    A binary search tree, constructed from Nodes
+
+    Attributes:
+        root(Node): The root node of the tree, which has no parents
+    
+    Methods:
+        is_empty(): Return True if empty, False otherwise
+        size(): Return the number of items in the tree
+        height(): Return the height of the tree, which is the length of the path from the root to its deepest leaf
+        add(item): Add item to its proper place in the tree, Return the modified tree
+        remove(item): Remove item from the tree, Return the modified tree
+        find(item): Return the matched item, If item is not in the tree, raise a ValueError
+        inorder(): Return a list with the data items in order of inorder traversal
+        preorder(): Return a list with the data items in order of preorder traversal
+        postorder(): Return a list with the data items in order of postorder traversal
+        rebalance(): rebalance the tree, Return the modified tree
+    """
+    
+    def is_empty(self):
+        """Return True if empty, False otherwise"""
+        pass
+
+    def size(self):
+        """Return the number of items in the tree"""
+        pass
+
+    def height(self):
+        """Return the height of the tree, which is the length of the path from the root to its deepest leaf"""
+        pass
+
+    def add(self, item):
+        """Add item to its proper place in the tree, Return the modified tree"""
+        pass
+
+    def remove(self, item):
+        """Remove item from the tree, Return the modified tree"""
+        pass
+
+    def find(self, item):
+        """Return the matched item; If item is not in the tree, raise a ValueError"""
+        pass
+
+    def inorder(self):
+        """Return a list with the data items in order of inorder traversal"""
+        pass
+
+    def preorder(self):
+        """Return a list with the data items in order of preorder traversal"""
+        pass
+
+    def postorder(self):
+        """Return a list with the data items in order of postorder traversal"""
+        pass
+
+    def rebalance(self):
+        """rebalance the tree, Return the modified tree"""
+        pass
