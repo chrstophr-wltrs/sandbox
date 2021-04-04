fff667e211da20096f4ce7b52ff42888ee61a082
spooky-noodle <christopherman2cool@gmail.com>
2021-04-03 23:47:32 -0600

    Implemented main()

--- python/School/Trees/main.py
+++ python/School/Trees/main.py
@@ -6,12 +6,14 @@ Date: 4/3/2021
 
 Description: Main function for testing the bst module
 
-Lessons Learned: I guess we'll see!
+Lessons Learned: I guess we'll see! 
+EDIT: Well, I hate recursion with every fiber of my being, and I want to die, but apart from that, valuable lessons were learned.
 
 '''
 from pathlib import Path
 from string import whitespace, punctuation
 from bst import BST
+from time import sleep
 
 
 class Pair:
@@ -76,11 +78,43 @@ def make_tree():
                             tree.add(pair)
     return tree
 
-
 def main():
-    ''' Program kicks off here.
-
+    ''' 
+    Program kicks off here.
     '''
+    tree = make_tree()
+    print("...")
+    sleep(4)
+    print("I worked")
+    sleep(2)
+    print("I worked so hard")
+    sleep(6)
+    print("I want")
+    sleep(3)
+    print("to cry")
+    sleep(5)
+    print("But it's finally done")
+    sleep(4)
+    print("Wait, you want to actually SEE something?")
+    sleep(3)
+    print("Uh...")
+    sleep(3)
+    print("I guess I can try ordering the tree around")
+    sleep(4)
+    print("Here goes")
+    sleep(3)
+    print("Pre order:")
+    sleep(3)
+    print(tree.preorder())
+    sleep(10)
+    print("In order:")
+    sleep(3)
+    print(tree.inorder())
+    sleep(10)
+    print("Post order:")
+    print(tree.postorder())
+    sleep(10)
+    print("Time for testing!")
     
 if __name__ == "__main__":
     main()
