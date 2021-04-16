cf16b411da4d21c2c4801cb2a1be003fc40a0def
Chris Walters <christopherman2cool@gmail.com>
2021-04-16 17:37:08 -0600

    add terminator and zsh

--- other/bash/chris_titus_terminal.sh
+++ other/bash/chris_titus_terminal.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+# Get terminator
+sudo apt-get update -q
+sudo apt-get install -q terminator
+
+# Get zsh
+sudo apt-get install -q zsh zsh-syntax-highlighting autojump zsh-autosuggestions
+
