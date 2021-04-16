7e187aa70a75b1baa16d8bf99ce84d365df63662
spooky-noodle <christopherman2cool@gmail.com>
2021-04-15 20:46:33 -0600

    final changes

--- other/bash/easy_linux_tips_mint_cinnamon_first_recommended.sh
+++ other/bash/easy_linux_tips_mint_cinnamon_first_recommended.sh
@@ -4,6 +4,8 @@ cd Desktop
 touch to-do.txt
 
 sudo chown -Rc $USER:$USER $HOME
+chmod -f 700 $HOME
+echo "Set proper permissions for $USER's home"
 
 echo "- LibreOffice" >> to-do.txt
 echo "  - Disable Java" >> to-do.txt
@@ -17,3 +19,35 @@ echo "    - System Reports" >> to-do.txt
 echo "    - mintwelcome" >> to-do.txt
 echo "    - Support for NVIDIA Prime" >> to-do.txt
 echo "    - Warpinator" >> to-do.txt
+echo "- Adjust Power Manager (Laptop Only)" >> to-do.txt
+echo "  - Right click the battery icon on the panel" >> to-do.txt
+
+echo "- Disable Mousepad while typing (Laptop Only)" >> to-do.txt
+echo "  - Menu > Preferences > Mouse and Touchpad > Touchpad > General" >> to-do.txt
+echo "    - Set 'Disable touchpad while typing' to OFF" >> to-do.txt
+
+sudo apt-get install -q xserver-xorg-input-synaptics
+sudo mkdir -q /etc/X11/xorg.conf.d 
+sudo cp /usr/share/X11/xorg.conf.d/70-synaptics.conf /etc/X11/xorg.conf.d/70-synaptics.conf
+
+echo "  - Menu > Preferences > Startup Applications > Add > Custom command" >> to-do.txt
+echo "    - Name: Syndaemon" >> to-do.txt
+echo "    - Command: syndaemon -i 1.0 -K -R -t" >> to-do.txt
+echo "    - Comment: Disable the touchpad while typing, with a reasonable delay of one second and only for tapping and scrolling" >> to-do.txt
+echo "    - Startup delay: 10 sec" >> to-do.txt
+
+sudo apt-get install -q dconf-editor
+
+echo "- Disable 'switch user'" >> to-do.txt
+echo "  - dconf-editor > org > cinnamon > desktop > lockdown" >> to-do.txt
+echo "  - set 'disable-user-switching' to ON" >> to-do.txt
+
+sudo apt-get install -q numlockx
+
+echo "- Turn Num Lock on automatically" >> to-do.txt
+echo "  - Menu > Administration > Login Window > Settings > Activate numlock" >> to-do.txt
+sudo apt-get remove -q gnome-orca
+
+echo "- Get Weather applet" >> to-do.txt
+echo "  - Right click panel > Applets > Download > Weather" >> to-do.txt
+echo "  - Applets > Manage > Weather > +" >> to-do.txt
