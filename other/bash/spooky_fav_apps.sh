ba19f8a249b6164d5bdf017e1ad339cd3dc7e489
spooky-noodle <christopherman2cool@gmail.com>
2021-04-15 23:31:26 -0600

    initial commit

---
+++ other/bash/spooky_fav_apps.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sudo apt-get install -q lame flac
+
+# Install Brave
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt-get update -q
+sudo apt-get install -q brave-browser
+sudo apt-get remove -q firefox
+
+# Install Spotify
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update -q && sudo apt-get install -q spotify-client
+
+# Install Git
+sudo apt-get install -q Git
+git config --global user.name "Chris Walters"
+git config --global user.email christopherman2cool@gmail.com
+
+# Install VS Code
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code
