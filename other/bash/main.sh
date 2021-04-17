d71aa220a1aed99697827bf05d42441d1dc8722e
Chris Walters <christopherman2cool@gmail.com>
2021-04-16 18:23:22 -0600

    initial commit

---
+++ other/bash/main.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo sh easy_linux_tips_mint_first_essential.sh
+sudo sh easy_linux_tips_mint_first_recommended.sh
+sudo sh spooky_fav_apps.sh
+sudo sh chris_titus_terminal.sh
+
+cd ~/Desktop
+touch to-do.txt
+
+echo "REMEMBER TO REBOOT TO FINALIZE CHANGES" >> to-do.txt
