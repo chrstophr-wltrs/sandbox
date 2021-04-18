a61942300057d60659276561f871ed3962435803
Chris Walters <christopherman2cool@gmail.com>
2021-04-17 21:56:14 -0600

    re-ordered blank person checking

--- python/School/Dictionaries/pyramid.py
+++ python/School/Dictionaries/pyramid.py
@@ -18,15 +18,15 @@ class HumanPyramid:
         Parameters:
             coords(tuple): tuple of 2 integers
         """
-        self.function_calls += 1
-        if coords == (0,0):
-            # top of the pyramid
-            return 0
         row = coords[0]
         col = coords[1]
         if (col < 0) or (col > row):
             # no person here
             return self.person_weight * -1
+        self.function_calls += 1
+        if coords == (0,0):
+            # top of the pyramid
+            return 0
         left, right = (row - 1, col - 1), (row - 1, col)
         l_weight = self.weight_on(left) + self.person_weight
         r_weight = self.weight_on(right) + self.person_weight
@@ -50,7 +50,7 @@ class HumanPyramid:
 
 def main():
     pyr = HumanPyramid()
-    pyr.test_recursive(sys.argv[1])
+    pyr.test_recursive(7)
 
 if __name__ == "__main__":
     main()
