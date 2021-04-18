cb2cd5723fe7968f3fd98607f56930e150e916db
Chris Walters <christopherman2cool@gmail.com>
2021-04-17 21:12:48 -0600

    started on HumanPyramid class

---
+++ python/School/Dictionaries/pyramid.py
@@ -0,0 +1,20 @@
+class HumanPyramid:
+    """
+    Handy little object to hold on to the methods and counters for recursive human pyramid experiments
+    """
+    def __init__(self):
+        self.function_calls = 0
+        self.cache_hits = 0
+        self.rec_flag = True
+        self.cache_flag = False
+    
+    def weight_on(self, coords:tuple):
+        """
+        returns the weight on a person 
+        """
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
