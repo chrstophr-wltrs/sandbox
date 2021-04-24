08a53b9170b13f30e0ccbb279a19bccdd9e43618
Chris Walters <christopherman2cool@gmail.com>
2021-04-24 12:06:00 -0600

    switching to mvc model

---
+++ javascript/CS 2550/MVC/Part A/scripts/formModel.js
@@ -0,0 +1,17 @@
+class Character {
+  constructor(
+    id = 0000,
+    name = "",
+    abilities = [0,0,0,0,0,0],
+    race = "",
+    profession = "",
+    sex = "",
+    hand = ""
+  ) {
+      this.id = id
+      this.name = name
+      this.abilities = {
+          str:
+      }
+  }
+}
