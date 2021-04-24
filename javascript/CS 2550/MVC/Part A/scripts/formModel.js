4f877cb2bafb42bf00897dde30f5f03b7472a40a
Chris Walters <christopherman2cool@gmail.com>
2021-04-24 12:09:26 -0600

    implemented character class

--- javascript/CS 2550/MVC/Part A/scripts/formModel.js
+++ javascript/CS 2550/MVC/Part A/scripts/formModel.js
@@ -2,16 +2,25 @@ class Character {
   constructor(
     id = 0000,
     name = "",
-    abilities = [0,0,0,0,0,0],
+    abilities = [0, 0, 0, 0, 0, 0],
     race = "",
     profession = "",
     sex = "",
-    hand = ""
+    leftHanded = ""
   ) {
-      this.id = id
-      this.name = name
-      this.abilities = {
-          str:
-      }
+    this.id = id;
+    this.name = name;
+    this.abilities = {
+      str: abilities[0],
+      dex: abilities[1],
+      con: abilities[2],
+      wis: abilities[3],
+      int: abilities[4],
+      cha: abilities[5],
+    };
+    this.race = race;
+    this.profession = profession;
+    this.sex = sex;
+    this.leftHanded = leftHanded;
   }
 }
