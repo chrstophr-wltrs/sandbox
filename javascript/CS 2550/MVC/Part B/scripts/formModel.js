a7a87b944ef326c0d04a13e23e54227c6e9a8e1c
Chris Walters <christopherman2cool@gmail.com>
2021-04-24 15:07:41 -0600

    initial commit

---
+++ javascript/CS 2550/MVC/Part B/scripts/formModel.js
@@ -0,0 +1,52 @@
+let currentCharacters = [];
+
+class Character {
+  constructor(
+    name = "",
+    abilities = [0, 0, 0, 0, 0, 0],
+    race = "",
+    profession = "",
+    sex = "",
+    leftHanded = false
+  ) {
+    this.name = name;
+    this.abilities = {
+      str: abilities[0],
+      dex: abilities[1],
+      con: abilities[2],
+      wis: abilities[3],
+      int: abilities[4],
+      cha: abilities[5],
+    };
+    this.race = race;
+    this.profession = profession;
+    this.sex = sex;
+    this.leftHanded = leftHanded;
+  }
+}
+
+const createItem = function (
+  name = "",
+  abilities = [],
+  race = "",
+  profession = "",
+  sex = "",
+  leftHanded = false
+) {
+  currentCharacters.push(
+    new Character(name, abilities, race, profession, sex, leftHanded)
+  );
+};
+
+const getAllItems = function () {
+  return currentCharacters;
+};
+
+const getItemByName = function (target) {
+  for (const i of currentCharacters) {
+    if (i.name == target) {
+      return i;
+    }
+  }
+  return undefined;
+};
