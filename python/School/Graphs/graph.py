25290c030ac6b50d2fa7308c6825f39a25b11c3c
Chris Walters <christopherman2cool@gmail.com>
2021-04-29 16:26:54 -0600

    initial commit

---
+++ python/School/Graphs/graph.py
@@ -0,0 +1,10 @@
+"""
+Project: Weighted Graphs
+Author: Chris Walters
+Course: CS 2420
+Date: 4/29/2021
+
+Description: Weighted graphs show up as a way to represent information in many applications, such as communication networks, water, power and energy systems, mazes, games and any problem where there is a measurable relationship between two or more things. It is therefore important to know how to represent graphs, and to understand important operations and algorithms associated with graphs. For this project, you will implement a directed, weighted graph and associated operations along with breadth-first search and Dijkstra's Shortest Path algorithms.
+
+For this project, you will write a Graph ADT and a small main function as a small test driver “application”. Include main() in your graph.py source file with conditional execution.  It is common for modules to include a runnable main() to use for testing purposes.  It happens in this case, you will have both main() AND the test code we give you to test your implementation.
+"""
