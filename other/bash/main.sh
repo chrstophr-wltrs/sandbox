366e42465df37f2341ff5844a39238553fd2aa84
Chris Walters <christopherman2cool@gmail.com>
2021-05-07 17:13:47 -0600

    added arc-theme.sh

--- other/bash/main.sh
+++ other/bash/main.sh
@@ -3,6 +3,7 @@
 sh easy_linux_tips_mint_cinnamon_first_essential.sh
 sh easy_linux_tips_mint_cinnamon_first_recommended.sh
 sh spooky_fav_apps.sh
+sh arc-theme.sh
 sh chris_titus_terminal.sh
 
 cd ~/Desktop
