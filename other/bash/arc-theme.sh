4ae60939dba74a565f616069c6fd1effa8df61dd
Chris Walters <christopherman2cool@gmail.com>
2021-05-07 17:11:57 -0600

    initial commit

---
+++ other/bash/arc-theme.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt install arc-theme
+
+cd ~/Desktop
+touch to-do.txt
+
+echo "- Install Futura" >> to-do.txt
+echo "  - https://freefontsdownload.net/free-futura_bk_bt-font-73014.htm" >> to-do.txt
