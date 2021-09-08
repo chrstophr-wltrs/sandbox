a633531848fcfdd5c5405de5d8f45e565f9527a6
Chris Walters <christopherman2cool@gmail.com>
2021-09-08 10:50:08 -0600

    created a sample login gui

--- python/pyqt_test/main.py
+++ python/pyqt_test/main.py
@@ -2,11 +2,15 @@ import sys
 from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtCore as qtc
 from PyQt5 import QtGui as qtg
+from PyQt5 import uic
 
-class MainWindow(qtw.QWidget):
+Ui_LoginForm, baseClass = uic.loadUiType('loginbox.ui')
+
+class MainWindow(baseClass):
     def __init__(self, *arg, **kwargs):
         super().__init__(*arg, **kwargs)
-
+        self.ui = Ui_LoginForm()
+        self.ui.setupUi(self)
         self.show()
 
 if __name__ == "__main__":
