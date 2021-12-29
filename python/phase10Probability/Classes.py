199b6c2b63d54c2c8cba0969bcc4a593e388c1d7
Chris Walters <christopherman2cool@gmail.com>
2021-12-28 21:08:26 -0700

    added constructor for Hand

--- python/phase10Probability/Classes.py
+++ python/phase10Probability/Classes.py
@@ -1,3 +1,6 @@
+import random as rnd
+rnd.seed()
+
 class Card:
     def __init__(self, color, value, duplicate):
         self.color = color
@@ -9,5 +12,16 @@ class Card:
 
 
 class Hand:
-    def __init__(self):
-        pass
+    def __init__(self, deck: list[Card], hand_size: int = 10):
+        self.number_cards = []
+        self.wilds = []
+        hand = []
+        while len(hand) < hand_size:
+            card = rnd.choice(deck)
+            deck.remove(card)
+            hand.append(card)
+        for card in hand:
+            if card.value == "Wild":
+                self.wilds.append(card)
+            else:
+                self.number_cards.append(card)
