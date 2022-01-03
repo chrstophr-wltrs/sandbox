c90c53ec87bb8c40880dc637b967c917a4489b18
Chris Walters <christopherman2cool@gmail.com>
2022-01-03 01:57:08 -0700

    run-detection successfully wraps around

--- python/phase10Probability/Testing.py
+++ python/phase10Probability/Testing.py
@@ -40,13 +40,13 @@ test_hands = [
     Hand().set(
         [
             Card("Red", "1", "a"),
-            Card("Red", "2", "a"),
-            Card("Red", "3", "b"),
-            Card("Yellow", "4", "a"),
-            Card("Yellow", "5", "a"),
-            Card("Green", "5", "b"),
-            Card("Green", "5", "b"),
-            Card("Blue", "5", "a"),
+            Card("Red", "4", "a"),
+            Card("Red", "6", "b"),
+            Card("Yellow", "7", "a"),
+            Card("Yellow", "7", "a"),
+            Card("Green", "10", "b"),
+            Card("Green", "10", "a"),
+            Card("Blue", "11", "a"),
             Card("Blue", "12", "b"),
             Card("Green", "12", "a"),
         ]
@@ -55,5 +55,5 @@ test_hands = [
 
 if __name__ == "__main__":
     print(f"Test Hand 1:")
-    print(f"\tValue Test 1: {test_hands[1].find_run(5)}")
+    print(f"\tValue Test 1: {test_hands[1].find_run(4)}")
     print(f"\tValue Test 2: {test_hands[1].find_set(3)}")
